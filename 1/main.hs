03a4306f14a1ec1235e61ccd580042cff3f7bbbf
alex <arch@battlestation.me>
2023-12-02 23:21:25 +0100

    challenge 1 done

---
+++ 1/main.hs
@@ -0,0 +1,15 @@
+computeNumberAux :: String -> String
+computeNumberAux [] = "0"
+computeNumberAux (x:xs) 
+            | [x] `elem` map show [0..9] = [x]
+            | otherwise = computeNumberAux xs
+
+computeNumber :: String -> Int
+computeNumber [] = 0
+computeNumber s = read (computeNumberAux s ++ computeNumberAux (reverse s))::Int
+
+main :: IO ()
+main = interact $ \input -> 
+  let output = sum $ map computeNumber (lines input)
+  in show output
+    
